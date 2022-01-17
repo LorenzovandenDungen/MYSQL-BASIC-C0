8c05e75f31d110482768fca5b598746e7752cc2c
LorenzovandenDungen <Lorenzo.vandendungen@student.rocva.nl>
2022-01-17 18:16:23 +0100

    Opdracht 07 primary keys

--- 03-Delete/Opdracht07-Primary-Keys/antwoorden.sql
+++ 03-Delete/Opdracht07-Primary-Keys/antwoorden.sql
@@ -1,5 +1,16 @@
+-- Opdracht 1
+SELECT * FROM players WHERE id = 198329
+-- Opdracht 2
+DELETE FROM players WHERE id = 198329
 -- Opdracht 3
 
--- Opdracht 4
+-- Opdracht 1
 
--- Opdracht 5
+-- Opdracht 2
+SELECT * FROM players WHERE id = 198329
+-- Opdracht 3
+DELETE FROM players WHERE id = 207494
+
+DELETE FROM players WHERE id = 226753
+
+Your SQL query has been executed successfully.
