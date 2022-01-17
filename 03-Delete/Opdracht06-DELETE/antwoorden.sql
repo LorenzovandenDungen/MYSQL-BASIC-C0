58d1f7c474d4085821348d92ff2217dc6f0905c3
LorenzovandenDungen <Lorenzo.vandendungen@student.rocva.nl>
2022-01-17 17:59:11 +0100

    DELETE (verbeterd)

--- 03-Delete/Opdracht06-DELETE/antwoorden.sql
+++ 03-Delete/Opdracht06-DELETE/antwoorden.sql
@@ -3,8 +3,8 @@ DELETE FROM players WHERE name = "David Silva"
 -- Opdracht 2 
 DELETE FROM players WHERE club = "Willem II"
 -- Opdracht 3
-DELETE FROM players WHERE name = "T. Vermaelen"
+DELETE FROM players WHERE nationality = "Belgium" AND club = "FC Barcelona"
 -- Opdracht 4
 DELETE FROM players WHERE age >= 38
 -- Opdracht 5
-DELETE FROM players WHERE id = 167905 
+DELETE FROM players WHERE id = 167905 OR id = 169595
